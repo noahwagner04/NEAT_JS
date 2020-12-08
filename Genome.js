04406659ab4c28ae5fdbceb279bd64c1dd4af838
Noah Wagner <wagnernoah@icloud.com>
2020-12-07 21:08:12 -0800

    added comments, could change

--- Genome.js
+++ Genome.js
@@ -1,52 +1,70 @@
 class Genome {
 	constructor(inNum, outNum, maxHidden, recur) {
+		// a list of all node genes in this genome
 		this.nodeG = [];
+		// a list of all connection genes in this genome
 		this.connectionG = [];
 
-		this.nodes = [];
-		this.connections = [];
+		// whether or not to allow for reccurent connections
+		this.recur = recur;
+		// the max hidden nodes that this genome can evolve
+		this.maxHidden = maxHidden;
 	}
 
+	// return a clone of this genome
 	clone() {
 
 	}
 
+	// initialize function, creates the initial node genes
+	// and connection genes
 	init() {
 
 	}
 
+	// creates a network from the node genes and 
+	// connection genes
 	constructNetwork() {
 
 	}
 
+	// adds a new connection to this genome
 	mutateAddConection(innovs) {
 
 	}
 
+	// adds a new node on a connection, splitting it in two
+	// and disabling it. Creates two new connections, the one leading
+	// to the new node(which recieves a weight of one 
+	// for minimal decrease in preformance) and the one going
+	// from the new node.
 	mutateAddNode(innovs) {
 
 	}
 
-	mutateWeights() {
+	// either perturbes a weight, or randomizes it.
+	// This is done to all connections.
+	mutateWeights(randomizeRate) {
 
 	}
 
-	perturbWeights(percent) {
+	// mutates the genome, combining all the mutate
+	// methods to this one method
+	mutate(addNodeRate, addConnectionRate, randomizeRate) {
 
 	}
 
-	randomizeWeights(percent){
-
-	}
-
-	mutate() {
-
-	}
-
+	// breeds two gemomes together using the NEAT
+	// competeing conventions solution
 	static breed(genome1, genome2, fit1, fit2) {
 
 	}
 
+	// compares two genomes compatibility to be in the same species
+	// using the NEAT compatibility method. It uses three global parameters
+	// c1, c2, and c3, which changes the importance of disjoint, excess, and wieght
+	// value. It uses the compatibility threshold global parameter to see
+	// whether they are in the same species.
 	static compatibility(genome1, genome2) {
 
 	}
--- Types.js
+++ Types.js
@@ -1,8 +1,20 @@
+// a node can be either a NEURON or a SENSOR
+// a NEURON is a node with incomming connections
+// for activation
+// a SENSOR is a node that is given a value to output,
+// (an input neuron)
 let nodeTypes = {
 	NEURON: 0,
 	SENSOR: 1
 };
 
+// different types of places a node can be in
+// in a network. Bias is used as a seprate input 
+// node. Its input value will always be the same, 1.
+// The computation is the same, but this method allows for
+// evolution to take care of either or not to add bias to a 
+// node, as it may not be necessary. It allows for a minimal
+// search space
 let nodePlaces =  {
 	INPUT: 0,
 	BIAS: 1,
