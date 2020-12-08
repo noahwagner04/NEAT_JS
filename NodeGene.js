d93bb4621b7fb17ab2dd040740640622c656ae36
Noah Wagner <wagnernoah@icloud.com>
2020-12-07 20:23:35 -0800

    added comments, could change

--- NodeGene.js
+++ NodeGene.js
@@ -1,7 +1,13 @@
+// gene representation of a node
 class NodeGene {
 	constructor(ntype, placement, id) {
+		// the id of this node gene, 
+		// helps with genetic encoding/mutation
 		this.id = id;
+		// the type of this node, either NEURON or SENSOR
 		this.ntype = ntype;
+		// the placement of this node, either
+		// BIAS, INPUT, HIDDEN, or OUTPUT
 		this.placement = placement;
 	}
 }
