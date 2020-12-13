f36a2b5dceb73691842aadb7e9e9284b91d78c3e
Noah Wagner <wagnernoah@icloud.com>
2020-12-12 18:59:09 -0800

    added netNode to node gene

--- NodeGene.js
+++ NodeGene.js
@@ -1,8 +1,9 @@
 // gene representation of a node
 class NodeGene {
 	constructor(config) {
+		this.netNode = undefined; 		   // a reference to the node this gene creates
 		this.id = config.id;
-		this.ntype = config.ntype; 		// the type of this node, either NEURON or SENSOR
+		this.ntype = config.ntype; 		   // the type of this node, either NEURON or SENSOR
 		this.placement = config.placement; // the placement of this node, either BIAS, INPUT, HIDDEN, or OUTPUT
 	}
 }
