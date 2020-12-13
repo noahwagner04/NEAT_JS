10edc35ef27eb5241d0b2a95fd6fadf2de9a2421
Noah Wagner <wagnernoah@icloud.com>
2020-12-12 19:03:52 -0800

    added comment to activation

--- Activation.js
+++ Activation.js
@@ -1,3 +1,15 @@
+/*
+static class that holds all
+the 6 activation functions, the user
+can either choose to use one of these functions
+for all activations in the population, or to 
+have individual nodes randomly choose their own
+activation, the net affect of this is to hopfully
+solve the issue of what activation function to use, 
+and to leave it to an evolutionary process to decide,
+it could also allow for more unique behavior
+*/
+
 class Activation {
 	static sigmoid(x, slope) {
 		return 1 / (1 + Math.exp(-(slope * x)));
