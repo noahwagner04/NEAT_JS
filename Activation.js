8e5073b7495e707229432e2c59d59f66563a4b96
Noah Wagner <wagnernoah@icloud.com>
2020-12-12 18:52:43 -0800

    finished all functions, got rid of softmax

--- Activation.js
+++ Activation.js
@@ -1,29 +1,25 @@
 class Activation {
 	static sigmoid(x, slope) {
-
+		return 1 / (1 + Math.exp(-(slope * x)));
 	}
 
 	static linearActivation(x) {
-
+		return x;
 	}
 
 	static binaryStep(x) {
-
+		return x < 0 ? 0 : 1;
 	}
 
 	static TanH(x) {
-
+		return (Math.exp(x) - Math.exp(-x)) / (Math.exp(x) + Math.exp(-x))
 	}
 
 	static ReLU(x) {
-
+		return x < 0 ? 0 : x;
 	}
 
-	static leakyRelU(x) {
-
-	}
-
-	static softmax(x) {
-
+	static leakyRelU(x, leakiness) {
+		return x < 0 ? x * leakiness : x;
 	}
 }
