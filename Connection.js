d38789354a90743fce189c4a71f926c9eb2d761a
Noah Wagner <wagnernoah@icloud.com>
2020-12-23 21:04:32 -0800

    added comments to top of script

--- Connection.js
+++ Connection.js
@@ -1,9 +1,15 @@
-// a connection between two nodes in a network
+/*
+a connection between two nodes in a network
+inNode and outNode can either be refs to node genes,
+in which case they are the connection within the connection 
+gene class, or a they can be refs to network nodes,
+in which case they live in the nodes in or out connection arrays
+*/
 class Connection {
 	constructor(config) {
 		this.weight = config.weight;	// the weighted value of this connection
-		this.inNode = config.inNode;	// a ref to the node inputing to this connection
-		this.outNode = config.outNode;  // a ref to the node this connection is inputing to
+		this.inNode = config.inNode;	// a ref to the node / node gene inputing to this connection
+		this.outNode = config.outNode;  // a ref to the node / node gene this connection is inputing to
 		this.isRecur = config.isRecur;  // whether or not this connection is recurrent
 	}
 }
