5858a4a59dca8491ce7f3c0c3f2c2ea8a38af3a7
Noah Wagner <wagnernoah@icloud.com>
2020-12-25 17:38:28 -0800

    added createNetNode func

--- NodeGene.js
+++ NodeGene.js
@@ -6,4 +6,16 @@ class NodeGene {
 		this.ntype = config.ntype; 		   // the type of this node, either NEURON or SENSOR
 		this.placement = config.placement; // the placement of this node, either BIAS, INPUT, HIDDEN, or OUTPUT
 	}
+
+	// creates a node with a given activation func
+	createNetNode(func) {
+		let node = new Node({
+			id: this.id,
+			type: this.ntype,
+			placement: this.placement,
+			activation: func
+		});
+		this.netNode = node;
+		return this;
+	}
 }
