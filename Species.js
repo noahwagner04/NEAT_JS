c1a1030be085e5a72aa722a9d631a74af3386527
Noah Wagner <wagnernoah@icloud.com>
2020-12-31 15:58:28 -0800

    added base class structure

---
+++ Species.js
@@ -0,0 +1,50 @@
+class Species {
+	constructor() {
+		this.aveFitness = 0;
+		this.maxFitness = 0;
+		this.maxFitnessEver = 0;
+		this.expectedOffspring = 0;
+		this.organisms = [];
+		this.lastImprovedAtAge = 0;
+	}
+
+	addOrganism(organism) {
+
+	}
+
+	adjustFitness() {
+
+	}
+
+	calcAverageFit() {
+
+	}
+
+	calcMaxFit() {
+
+	}
+
+	countOffspring() {
+
+	}
+
+	removeOrganism(organism) {
+
+	}
+
+	getChamp() {
+
+	}
+
+	reproduce(gen, population, sortedSpecies) {
+
+	}
+
+	rank() {
+
+	}
+
+	checkCompatibility(dist) {
+
+	}
+}
