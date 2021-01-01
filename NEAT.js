ef83de8494c963c6e88aa412a1986ec051089932
Noah Wagner <wagnernoah@icloud.com>
2021-01-01 10:15:58 -0800

    finnished outline of class

---
+++ NEAT.js
@@ -0,0 +1,33 @@
+class NEAT {
+	constructor(config) {
+
+	}
+
+	nextGen() {
+
+	}
+
+	setFit(fitness, index) {
+
+	}
+
+	getWinner() {
+
+	}
+
+	setInputs(inputArray, index) {
+
+	}
+
+	getOutputs(index) {
+
+	}
+
+	getAllOutputes() {
+
+	}
+
+	feedForward() {
+		
+	}
+}
