8ec1d533abbe323184fc428041f8bfc9f5785ee3
Noah Wagner <wagnernoah@icloud.com>
2021-01-01 10:16:27 -0800

    added outline of class

---
+++ Population.js
@@ -0,0 +1,50 @@
+class Population {
+	constructor(genome, size) {
+		this.genomes = [];
+		this.first = genome;
+		this.size = size;
+		this.species = [];
+		this.innovations = [];
+		this.currInnov = 0;
+		this.currNodeId = 0;
+		this.champ = undefined;
+		this.gen = 0;
+
+		this.highestFit = 0;
+		this.aveFit = 0;
+
+		this.winner = undefined;
+	}
+
+	initPop() {
+
+	}
+
+	speciateOrg(organism) {
+
+	}
+
+	speciateAll() {
+
+	}
+
+	calcNextGen() {
+
+	}
+
+	rankSpeciesOrgs() {
+
+	}
+
+	getChamp() {
+
+	}
+
+	getChamp() {
+
+	}
+
+	updateWinner(champ) {
+		
+	}
+}
