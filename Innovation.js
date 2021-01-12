032610af3fd9ed822a201f665d308dac99ad2478
Noah Wagner <wagnernoah@icloud.com>
2021-01-11 20:48:57 -0800

    changed comments

--- Innovation.js
+++ Innovation.js
@@ -9,8 +9,8 @@ class Innovation {
 			this.inNodeId = inNodeId;
 			this.outNodeId = outNodeId;
 
-			this.innovation = innov1;   // the number assigned to this innov
-			this.innovation2 = args[0]; // only used if this is a node mutation, both innovs act as innov nums for two new connections
+			this.innovation = innov1;   // the innov number of the connection leading to this node
+			this.innovation2 = args[0]; // the innov number of the connection leading out of this node
 
 			this.nodeId = args[1];	    // new id of node, only used if this is a node mutation
 			this.oldInnov = args[2];    // only used if node mutation, innov number of the connection this node is splitting
@@ -21,7 +21,7 @@ class Innovation {
 			this.inNodeId = inNodeId;
 			this.outNodeId = outNodeId;
 
-			this.innovation = innov1;
+			this.innovation = innov1; // innov of this new connection
 
 			this.newWeight = args[0];  // only used if this is a connection mutation, weight of the connection
 			this.recur = args[1]; 	   // recurrent flag of new connection, only used of this is a connection mutation
