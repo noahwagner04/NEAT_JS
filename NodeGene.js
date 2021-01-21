97c851127a269bffe0ea5092ec5d22d5705d32e1
Noah Wagner <wagnernoah@icloud.com>
2021-01-20 20:30:28 -0800

    added node activation in node gene

--- NodeGene.js
+++ NodeGene.js
@@ -1,18 +1,20 @@
 // gene representation of a node
 class NodeGene {
-	constructor(id, ntype, placement) {
+	constructor(id, ntype, placement, netNodeActivation) {
 		if(arguments.length === 1 && arguments[0] instanceof NodeGene) { // check to see of recieved nodeGene, if so, exicute copy constructor
 			let nodeGene = arguments[0];
 			this.netNode = undefined; // a reference to the node this gene creates
 			this.id = nodeGene.id;
 			this.ntype = nodeGene.ntype; 		     // the type of this node, either NEURON or SENSOR
 			this.placement = nodeGene.placement;     // the placement of this node, either BIAS, INPUT, HIDDEN, or OUTPUT
+			this.netNodeActivation = nodeGene.netNodeActivation;
 		} else {
 			this.netNode = undefined; 	   // a reference to the node this gene creates
 			this.duplicate = undefined;		   // used for genome duplication
 			this.id = id;
 			this.ntype = ntype; 		   // the type of this node, either NEURON or SENSOR
 			this.placement = placement;    // the placement of this node, either BIAS, INPUT, HIDDEN, or OUTPUT
+			this.netNodeActivation = netNodeActivation;	// used to keep track of node activation when choosing randomly
 		}
 	}
 
@@ -20,11 +22,8 @@ class NodeGene {
 	creates a node with a given activation func,
 	takes population obj to get activation func
 	*/
-	createNetNode(population) {
-		let activationFunc = undefined;
-		if(population.NEAT.randomActivation) activationFunc = NodeGene.chooseActivationFunc;
-		else activationFunc = population.NEAT.activationFunction;
-		this.netNode = new Node(this.id, this.ntype, this.placement, activationFunc);
+	createNetNode() {
+		this.netNode = new Node(this.id, this.ntype, this.placement, this.netNodeActivation);
 		return this;
 	}
 
@@ -34,17 +33,5 @@ class NodeGene {
 		let newNodeGene = new NodeGene(this);
 		this.duplicate = newNodeGene;
 		return newNodeGene;
-	}
-
-	/*
-	chooses from the activationTypes array,
-	which has the six default activation functions
-	in it, the user can add their own functions to the
-	array and this function will have no problem with 
-	ranomly picking it
-	*/
-	static chooseActivationFunc() {
-		let rdmIndex = Math.floor(Math.random() * activationTypes.length);
-		return activationTypes[rdmIndex];
 	}
 }
