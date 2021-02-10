5b148cc890a408a195602f71f6c43c85a302af74
Noah Wagner <wagnernoah@icloud.com>
2021-02-09 17:06:29 -0800

    added comments

--- Population.js
+++ Population.js
@@ -1,3 +1,10 @@
+/*
+this class holds all the organisms in 
+one place, user can choose to choose 
+to interact with this class directly, 
+or to use the NEAT class for more friendly
+interface
+*/
 class Population {
 	constructor(genome, size, NEAT) {
 		this.organisms = [];
@@ -17,30 +24,37 @@ class Population {
 		this.winner = undefined;
 	}
 
+	// initializes the population from one genome, speciates them
 	initPop() {
 
 	}
 
+	// speciates an organism to a species it matches with (dont this this will exist)
 	speciateOrg(organism) {
 
 	}
 
+	// speciates all organisms into species (initializes species)
 	speciateAll() {
 
 	}
 
+	// main reproduction loop, makes the next gen
 	calcNextGen() {
 
 	}
 
+	// ranks all species organisms within their species
 	rankSpeciesOrgs() {
 
 	}
 
+	// get the best preforming organism
 	getChamp() {
 
 	}
 
+	// make winner be the best preforming org of all time
 	updateWinner(champ) {
 
 	}
