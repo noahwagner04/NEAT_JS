ef8e2ee556fae8316a3b30f81ec99d569f5d73e9
Noah Wagner <wagnernoah@icloud.com>
2021-02-09 17:07:22 -0800

    added new param, added comments

--- NEAT.js
+++ NEAT.js
@@ -1,3 +1,10 @@
+/*
+this is the main class the user
+will be interacting with (or population if preferred)
+it holds all the main parameters for the algorithm
+you can instantiate multiple neat instances or multiple
+population instances that share the same NEAT config if desired
+*/
 class NEAT {
 	constructor(config) {
 		this.disjointCoefficient = config.compatibility.disjointCoefficient;
@@ -27,35 +34,48 @@ class NEAT {
 
 		this.inputs = config.network.inputs;
 		this.outputs = config.network.outputs;
+		this.singleTimeStepActivation = config.network.singleTimeStepActivation;
 		this.activationFunction = config.network.activationFunction;
 		this.randomActivation = config.network.randomActivation;
 	}
 
+	// calculate the next generation
 	nextGen() {
 
 	}
 
+	// sets the fitness of a specified organism at a given index
 	setFit(fitness, index) {
 
 	}
 
+	// gets the best preforming organism
 	getWinner() {
 
 	}
 
+	// sets the inputs to an organisms brain with a given index and a input array
 	setInputs(inputArray, index) {
 
 	}
 
+	// gets the output of an organism at a given index
 	getOutputs(index) {
 
 	}
 
-	getAllOutputes() {
+	// returns all the organisms outputs in a 2d array
+	getAllOutputs() {
 
 	}
 
+	// activates all organisms networks
 	feedForward() {
+
+	}
+
+	// activates one organisms network
+	feedForwardOrg(index) {
 
 	}
 }
